8c58b4d3c490f8791b16445350329f572130a6b0
Paul S. Strauss <pss@acm.org>
2020-09-26 18:10:06 -0700

    Recreating MakerVR-dist doc in gh-pages branch.

---
+++ 0.0.2/js/versions.js
@@ -0,0 +1,24 @@
+
+var project_names = ['MakerVR', 'MakerVR-dist'];
+var versions = [
+    {
+        "version": "0.0.1",
+        "aliases": [],
+    },
+    {
+        "version": "0.0.2",
+        "aliases": [],
+    },
+    {
+        "version": "0.0.3",
+        "aliases": [],
+    },
+    {
+        "version": "0.0.4",
+        "aliases": ['latest'],
+    },
+    {
+        "version": "0.0.5",
+        "aliases": ['dev'],
+    },
+]
